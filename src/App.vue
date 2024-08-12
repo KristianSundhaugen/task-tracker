6b1d74c7fa02c0ec0428be4235b5b20c0c409471
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-08-12 16:35:47 +0200

    feat: Added AddTask functionality. Togheter with possibility to toggle AddTask on and off

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <AppHeader title="Hello" />
+    <AppHeader @toggle-add-task="toggleAddTask" :showAddTask="showAddTask" title="Task Tracker" />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
@@ -8,19 +11,28 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import Tasks from './components/Tasks.vue';
+import AddTask from './components/AddTask.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    Tasks
+    Tasks,
+    AddTask
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
     deleteTask(id) {
       if(confirm('Are you sure?')){
         this.tasks = this.tasks.filter((task) => task.id !== id)
