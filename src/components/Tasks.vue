e2aaa384593e81977b62b497532a28294262eadd
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-08-12 15:34:29 +0200

    add: Intial project commit. Added some functionality for viewing dummy tasks

---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <Task :task="task" />
+    </div>
+</template>
+
+<script>
+  import Task from './Task'
+
+  export default {
+    name: 'TaskList',
+    props: {
+      tasks: Array
+    },
+    components: {
+      Task,
+    },
+  }
+</script>
